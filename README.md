87f656cf74a7d524b647022b1e7e5a33b5f97642
Ryan Grey <ryan.g.grey@gmail.com>
2022-07-10 14:41:33 +0100

    Add board and spawn tetrominoes in correct location
    Also remove all the random tetrominoes created at the top

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ Color of blocks taken from https://en.wikipedia.org/wiki/Tetromino#One-sided_tet
 ## Gravity
 
 Gravity per level taken from https://gamedev.stackexchange.com/questions/159835/understanding-tetris-speed-curve. Where 1G = 1 cell per frame.
+
+## Spawning
+
+Spawning attempts to follow rules here - https://tetris.fandom.com/wiki/SRS#Spawn_Orientation_and_Location.
+
+However the implementation is simplified:
+
+- Spawn in rows 20 and 19. Not sure what the ramifications of this is yet, but it simplifies things as we don't need to handle spawining above the game board (22/21) and only rendering once entering the board (20/10). This might need to change later.
+- Spawn in column 4. All tetrominoes are the correct width to end up adhering to the rules in the above link.
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,9 +1,8 @@
 import Phaser from "phaser";
 import { colors } from "../colors";
 
-const minoHeight = 20;
-const minoWidth = 20;
-const boardY = 0;
+const minoHeight = 25;
+const minoWidth = 25;
 const boardColumns = 10;
 const boardRows = 20;
 
@@ -23,55 +22,25 @@ class Game extends Phaser.Scene {
   }
 
   create(data) {
-    // draw tetrominos
-
-    const tetrominoes = this.cache.json.get("tetrominoes");
-    const tetrominoColors = this.cache.json.get("tetrominoColors");
-    this.createTetromino(
-      tetrominoes.i[0],
-      { x: 0, y: 0 },
-      tetrominoColors["i"],
-      colors.hexBlack
-    );
-
-    this.createTetromino(
-      tetrominoes.j[0],
-      { x: 80, y: 0 },
-      tetrominoColors["j"],
-      colors.hexBlack
-    );
-    this.createTetromino(
-      tetrominoes.l[0],
-      { x: 140, y: 0 },
-      colors.hexOrange,
-      colors.hexBlack
-    );
-    this.createTetromino(
-      tetrominoes.o[0],
-      { x: 200, y: 0 },
-      colors.hexYellow,
-      colors.hexBlack
-    );
-    this.createTetromino(
-      tetrominoes.s[0],
-      { x: 280, y: 0 },
-      colors.hexGreen,
-      colors.hexBlack
-    );
-    this.createTetromino(
-      tetrominoes.t[0],
-      { x: 340, y: 0 },
-      colors.hexPink,
-      colors.hexBlack
-    );
-    this.createTetromino(
-      tetrominoes.z[0],
-      { x: 400, y: 0 },
-      colors.hexRed,
-      colors.hexBlack
-    );
-
+    this.createBoard();
     this.tetromino = this.createRandomTetromino();
+  }
+
+  createBoard() {
+    const boardWidth = boardColumns * minoWidth;
+    const boardHeight = boardRows * minoHeight;
+    const baseSize = this.scale.baseSize;
+    const x = Math.floor(baseSize.width / 2 - boardWidth / 2);
+    const y = Math.floor(baseSize.height / 2 - boardHeight / 2);
+
+    this.board = this.add.rectangle(
+      x,
+      y,
+      boardColumns * minoWidth,
+      boardRows * minoHeight
+    );
+    this.board.setOrigin(0);
+    this.board.setStrokeStyle(1, colors.hexBlack);
   }
 
   createRandomTetromino() {
@@ -84,9 +53,13 @@ class Game extends Phaser.Scene {
     const tetrominoRotations = randomTetrominoEntry[1];
     const tetrominoJSON = tetrominoRotations[0];
     const tetrominoColor = tetrominoColors[tetrominoName];
+
+    // spawn in top 2 rows, at column index 3 (4th column)
+    const x = this.board.x + 3 * minoWidth;
+    const y = this.board.y + 0 * minoHeight;
     const randomTetromino = this.createTetromino(
       tetrominoJSON,
-      { x: 0, y: 0 },
+      { x, y },
       tetrominoColor,
       colors.hexBlack
     );
@@ -97,8 +70,8 @@ class Game extends Phaser.Scene {
     const container = this.add.container();
     tetromino.forEach((row, yIndex) => {
       row.forEach((bit, xIndex) => {
-        const x = coord.x + minoWidth / 2 + xIndex * minoWidth;
-        const y = coord.y + minoHeight / 2 + yIndex * minoHeight;
+        const x = coord.x + xIndex * minoWidth;
+        const y = coord.y + yIndex * minoHeight;
         if (bit) {
           const mino = this.createMino(
             x,
@@ -118,22 +91,24 @@ class Game extends Phaser.Scene {
   createMino(x, y, width, height, fillColor, strokeColor) {
     const mino = this.add.rectangle(x, y, width, height, fillColor);
     mino.setStrokeStyle(1, strokeColor);
+    mino.setOrigin(0);
     return mino;
   }
 
   update(time, delta) {
     // assuming 60fps
 
+    // stop tetromino if it hits the bottom
     const bottomOfTetromino = this.tetromino.getBounds().bottom;
-    const bottomOfBoard = boardY + boardRows * minoHeight;
+    const bottomOfBoard = this.board.y + boardRows * minoHeight;
     if (bottomOfTetromino >= bottomOfBoard) {
       this.yDelta = 0;
       this.tetromino = this.createRandomTetromino();
     }
+
+    // apply gravity, where 1G = 1 mino per frame
     const gravityJson = this.cache.json.get("gravity");
     const gravity = gravityJson[this.level];
-
-    // 1G = 1 mino per frame
     this.yDelta += gravity * minoHeight;
     if (this.yDelta >= minoHeight) {
       const quotient = Math.floor(this.yDelta / minoHeight);
