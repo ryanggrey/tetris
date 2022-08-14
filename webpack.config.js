03e3409ae91bb4f4ccfd5aa8c21a38f998cd8ec0
Ryan Grey <ryan.g.grey@gmail.com>
2022-08-14 22:46:40 +0100

    Formatting webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,56 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = (env, argv) => ({
   entry: {
-    app: path.resolve(__dirname, './src/index.js'),
-    vendor: ['phaser'],
+    app: path.resolve(__dirname, "./src/index.js"),
+    vendor: ["phaser"],
   },
-  devtool: argv.mode === 'development' ? 'eval-source-map' : 'none',
+  devtool: argv.mode === "development" ? "eval-source-map" : "none",
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: "babel-loader",
       },
       {
         test: /\.html$/,
         use: {
-          loader: 'html-loader',
+          loader: "html-loader",
           options: {
-            minimize: argv.mode === 'production',
+            minimize: argv.mode === "production",
           },
         },
       },
       {
         test: [/\.vert$/, /\.frag$/],
-        use: 'raw-loader',
+        use: "raw-loader",
       },
     ],
   },
   plugins: [
     new webpack.DefinePlugin({
-      DEV: argv.mode === 'development',
+      DEV: argv.mode === "development",
       WEBGL_RENDERER: true,
       CANVAS_RENDERER: true,
     }),
     new CleanWebpackPlugin(),
-    new CopyWebpackPlugin([
-      { from: 'assets', to: 'assets' },
-    ]),
+    new CopyWebpackPlugin([{ from: "assets", to: "assets" }]),
     new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
-      chunks: ['vendor', 'app'],
-      chunksSortMode: 'manual',
+      template: "./src/index.html",
+      filename: "./index.html",
+      chunks: ["vendor", "app"],
+      chunksSortMode: "manual",
     }),
   ],
   optimization: {
     splitChunks: {
-      name: 'vendor',
-      chunks: 'all',
+      name: "vendor",
+      chunks: "all",
     },
   },
 });
